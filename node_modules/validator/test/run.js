2d2f4a08e6229ac0012685d92284d383c2602735
Cole Potrocky <cole@colepotrocky.com>
2012-09-15 02:16:51 -0400

    Removed node_modules.

--- node_modules/validator/test/run.js
+++
@@ -1,8 +0,0 @@
-var validatorTests = require('./validator.test.js');
-for (test in validatorTests) {
-  validatorTests[test]();
-}
-var filterTests = require('./filter.test.js');
-for (test in filterTests) {
-  filterTests[test]();
-}
