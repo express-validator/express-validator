d2eb638611a015dd04d4aa59c4c790ca324bcc66
Rusty Bailey <rustylbailey@gmail.com>
2015-04-29 16:58:54 -0400

    Add mocha/chai as dependencies, simplify app.js helper, create mocha tests for optional().

--- test/helpers/app.js
+++ test/helpers/app.js
@@ -1,20 +1,17 @@
 // Sample app
 var express = require('express');
 var expressValidator = require('../../index');
+var bodyParser = require('body-parser');
+var http = require('http');
 
-function App(port, validation) {
-  this.app = null;
-  this.port = port;
-  this.validation = validation;
-}
-module.exports = App;
+var port = process.env.PORT || 8888;
+var app = express();
 
-App.prototype.start = function() {
-  var self = this;
-  self.app = express.createServer();
+module.exports = function(validation) {
 
-  self.app.use(express.bodyParser());
-  self.app.use(expressValidator({
+  app.set('port', port);
+  app.use(bodyParser.json());
+  app.use(expressValidator({
     customValidators: {
       isArray: function(value) {
         return Array.isArray(value);
@@ -22,13 +19,55 @@ App.prototype.start = function() {
     }
   }));
 
-  self.app.get(/\/test(\d+)/, self.validation);
-  self.app.get('/:testparam?', self.validation);
-  self.app.post('/:testparam?', self.validation);
+  app.get(/\/test(\d+)/, validation);
+  app.get('/:testparam?', validation);
+  app.post('/:testparam?', validation);
 
-  self.app.listen(this.port);
+  /**
+   * Create HTTP server.
+   */
+  var server = http.createServer(app);
+
+  /**
+   * Listen on provided port, on all network interfaces.
+   */
+  server.listen(port);
+  server.on('error', onError);
+  server.on('listening', onListening);
+
+  return app;
 };
 
-App.prototype.stop = function() {
-  this.app.close();
-};
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
