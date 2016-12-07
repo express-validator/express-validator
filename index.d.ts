f169a2d081ce57440213fef9cca8b2188fb6641c
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2016-12-07 17:39:40 +0100

    If a schema is passed, the validator should not accept a message

--- index.d.ts
+++ index.d.ts
@@ -34,7 +34,8 @@ declare namespace ExpressValidator {
   export type MobilePhoneLocal = 'ar-DZ' | 'ar-SA' | 'ar-SY' | 'cs-CZ' | 'de-DE' | 'da-DK' | 'el-GR' | 'en-AU' | 'en-GB' | 'en-HK' | 'en-IN' | 'en-NZ' | 'en-US' | 'en-CA' | 'en-ZA' | 'en-ZM' | 'es-ES' | 'fi-FI' | 'fr-FR' | 'hu-HU' | 'it-IT' | 'ja-JP' | 'ms-MY' | 'nb-NO' | 'nn-NO' | 'pl-PL' | 'pt-PT' | 'ru-RU' | 'sr-RS' | 'tr-TR' | 'vi-VN' | 'zh-CN' | 'zh-TW'
 
 
-  interface ValidatorFunction { (item: string | {}, message?: string): Validator; }
+  interface ValidatorFunction { (item: string | string[], message?: string): Validator; }
+  interface ValidatorFunction { (schema: {}): Validator; }
   /**
    * This one's used for RegexRoutes
    * @see https://github.com/ctavan/express-validator#regex-routes
