e2370636c25e1354d355f3d46f7161c30aa568c9
Azad Ratzki <taicho@gmail.com>
2017-06-11 09:54:51 -0400

    Fix for TypeScript Typings
    Without this fix users of TypeScript (at least 2.3.4) unable to import
    the library using standard “import” syntax without either a) removing
    the typings or b) using “require”.

--- index.d.ts
+++ index.d.ts
@@ -10,19 +10,15 @@
 declare namespace Express {
   interface Request extends ExpressValidator.RequestValidation {}
 }
-
-// External express-validator module.
-declare module "express-validator" {
   import express = require('express');
 
   /**
    * @param options see: https://github.com/ctavan/express-validator#middleware-options
    * @constructor
    */
-  function ExpressValidator(options?: ExpressValidator.Options.ExpressValidatorOptions): express.RequestHandler;
+  declare function ExpressValidator(options?: ExpressValidator.Options.ExpressValidatorOptions): express.RequestHandler;
 
   export = ExpressValidator;
-}
 
 // Internal Module.
 declare namespace ExpressValidator {
