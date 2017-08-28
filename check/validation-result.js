1eaf36f11731a4a00c53b6683c723a6eb7635e16
Gustavo Henke <guhenke@gmail.com>
2017-08-28 14:37:05 -0300

    check/validationResult: add .formatWith()
    This will allow to change the shape of the errors
    before returning them.

--- check/validation-result.js
+++ check/validation-result.js
@@ -1,10 +1,12 @@
 module.exports = req => decorateAsValidationResult({}, req._validationErrors);
 
 function decorateAsValidationResult(obj, errors) {
+  let formatter = error => error;
+
   obj.isEmpty = () => !errors.length;
   obj.array = ({ onlyFirstError } = {}) => {
     const used = {};
-    return !onlyFirstError ? errors : errors.filter(error => {
+    let filteredErrors = !onlyFirstError ? errors : errors.filter(error => {
       if (used[error.param]) {
         return false;
       }
@@ -12,19 +14,26 @@ function decorateAsValidationResult(obj, errors) {
       used[error.param] = true;
       return true;
     });
+
+    return filteredErrors.map(formatter);
   };
 
   obj.mapped = () => errors.reduce((mapping, error) => {
     if (!mapping[error.param]) {
-      mapping[error.param] = error;
+      mapping[error.param] = formatter(error);
     }
 
     return mapping;
   }, {});
 
+  obj.formatWith = errorFormatter => {
+    formatter = errorFormatter;
+    return obj;
+  };
+
   obj.throw = () => {
     if (errors.length) {
-      throw decorateAsValidationResult(new Error('Validation failed'), errors);
+      throw decorateAsValidationResult(new Error('Validation failed'), errors).formatWith(formatter);
     }
   };
 
