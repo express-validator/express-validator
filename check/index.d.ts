d066703451cfdd1c4387699b80ee1262d15d8337
Gustavo Henke <guhenke@gmail.com>
2018-01-18 13:43:24 +1100

    check: add more typings tests

--- check/index.d.ts
+++ check/index.d.ts
@@ -1,4 +1,9 @@
-export { Validator, ValidationChain, ValidatorOptions } from './check';
+export {
+  CustomValidator,
+  Validator,
+  ValidationChain,
+  ValidatorOptions,
+} from './check';
 export * from './one-of';
 export * from './validation-chain-builders';
 export * from './validation-result';
---
+++ check/index.spec.ts
@@ -0,0 +1,26 @@
+import {
+  // check.d.ts
+  CustomValidator,
+  Validator,
+  ValidationChain,
+  ValidatorOptions,
+
+  // validation-chain-builders.d.ts
+  ValidationChainBuilder,
+  check,
+  body,
+  cookie,
+  header,
+  param,
+  query,
+
+  // oneOf.d.ts
+  oneOf,
+
+  // validation-result.d.ts
+  validationResult,
+  ErrorFormatter,
+  Result,
+  ResultFactory,
+  ResultFactoryBuilderOptions,
+} from './index';
