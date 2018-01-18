a3d967300ae1675eec216d836e3c3666c903fb5a
Gustavo Henke <guhenke@gmail.com>
2018-01-18 18:14:40 +1100

    Revamp TypeScript usage throughout the codebase
    Closes #496

--- check/index.d.ts
+++ check/index.d.ts
@@ -1,25 +1,9 @@
-import * as express from 'express';
-import { Validator } from '../shared-typings';
-
-export type ValidationChains = ValidationChain | ValidationChain[];
-
-export const check: ValidationChainBuilder;
-export const body: ValidationChainBuilder;
-export const cookie: ValidationChainBuilder;
-export const header: ValidationChainBuilder;
-export const param: ValidationChainBuilder;
-export const query: ValidationChainBuilder;
-export { default as validationResult } from './validation-result';
-export function oneOf (chains: ValidationChains[], message?: string): express.RequestHandler;
-
-export interface ValidationChainBuilder {
-  (field: string | string[], message?: string): ValidationChain;
-}
-
-export interface ValidationChain extends express.RequestHandler, Validator {
-  custom(validator: CustomValidator): this;
-}
-
-export interface CustomValidator {
-  (value: any, options: { req: express.Request, location: string, path: string }): any;
-}
+export {
+  CustomValidator,
+  Validator,
+  ValidationChain,
+  ValidatorOptions,
+} from './check';
+export * from './one-of';
+export * from './validation-chain-builders';
+export * from './validation-result';
--- filter/index.d.ts
+++ filter/index.d.ts
@@ -1,20 +1,3 @@
-import * as express from 'express';
-import { Sanitizer, Location } from '../shared-typings';
-
-export function matchedData (req: express.Request, options?: MatchedDataOptions): { [key: string]: any };
-export const sanitize: SanitizationChainBuilder;
-export const sanitizeBody: SanitizationChainBuilder;
-export const sanitizeCookie: SanitizationChainBuilder;
-export const sanitizeParam: SanitizationChainBuilder;
-export const sanitizeQuery: SanitizationChainBuilder;
-
-interface MatchedDataOptions {
-  onlyValidData?: boolean
-  locations?: Location[]
-}
-
-export interface SanitizationChainBuilder {
-  (field: string | string[]): SanitizationChain;
-}
-
-export interface SanitizationChain extends express.RequestHandler, Sanitizer {}
+export * from './matched-data';
+export { Sanitizer, SanitizationChain } from './sanitize';
+export * from './sanitization-chain-builders';
