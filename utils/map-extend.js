76809142b339ea84a080d91dea3e21d0216e00d3
Gustavo Henke <guhenke@gmail.com>
2018-03-29 22:05:01 +1100

    legacy: move around utils that aren't used by other APIs

--- utils/map-extend.js
+++
@@ -1,5 +0,0 @@
-module.exports = function mapAndExtend(src, dest, mapFunction) {
-  Object.keys(src).forEach(function (name) {
-    dest[name] = mapFunction(name, src);
-  });
-};
--- utils/replace-args.js
+++
@@ -1,7 +0,0 @@
-module.exports = function replaceArgs(msg, args) {
-  if (typeof msg !== 'string') {
-    return msg;
-  }
-
-  return args.reduce((msg, arg, index) => msg.replace('%' + index, arg), msg);
-};
