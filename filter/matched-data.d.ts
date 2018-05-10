e589b1be68df34164493eff7c3d9edba996792b3
Gustavo Henke <guhenke@gmail.com>
2018-05-10 23:08:48 +1000

    Add includeOptionals option

--- filter/matched-data.d.ts
+++ filter/matched-data.d.ts
@@ -4,6 +4,7 @@ import { Location } from '../check/location';
 export function matchedData(req: express.Request, options?: Partial<MatchedDataOptions>): Record<string, any>;
 
 export interface MatchedDataOptions {
+  filterOptionals: boolean;
   onlyValidData: boolean;
   locations: Location[];
 }
--- filter/matched-data.spec.ts
+++ filter/matched-data.spec.ts
@@ -4,6 +4,7 @@ let dataRecord: Record<string, any> = matchedData({} as any);
 dataRecord = matchedData({} as any, {});
 dataRecord = matchedData({} as any, {
   locations: ['body', 'cookies'],
+  filterOptionals: true,
   onlyValidData: true
 });
 
