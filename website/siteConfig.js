4528f64d1a782240da7a7a5846538531930c58ca
Gustavo Henke <guhenke@gmail.com>
2018-05-15 20:37:40 +1000

    Add CNAME and clean up siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,77 +1,40 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config.html for all the possible
-// site configuration options.
-
+// See https://docusaurus.io/docs/site-config.html for all the possible site configuration options.
 const siteConfig = {
-  title: 'express-validator' /* title for your website */,
+  // Metadata
+  title: 'express-validator',
   tagline: 'express-validator docs',
-  url: 'https://express-validator.github.io' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
-
-  // Used for publishing and more
+  url: 'https://express-validator.js.org',
+  baseUrl: '/',
+  cname: 'express-validator.js.org',
   projectName: 'express-validator',
   organizationName: 'express-validator',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
 
-  // For no header links in the top nav bar -> headerLinks: [],
+  // Website customization
   headerLinks: [
     {doc: 'index', label: 'Docs'},
     {doc: 'check-api', label: 'API'},
     {href: 'https://github.com/express-validator/express-validator', label: 'GitHub'},
   ],
-
-  /* path to images for header/footer */
   headerIcon: 'img/logo.svg',
   footerIcon: 'img/logo.svg',
   favicon: 'img/favicon.png',
-
-  /* colors for website */
   colors: {
     primaryColor: 'rgb(107, 0, 177)',
     secondaryColor: 'rgb(93, 0, 154)',
   },
+  onPageNav: 'separate',
+  scripts: ['https://buttons.github.io/buttons.js'],
 
+  // Features
   editUrl: 'https://github.com/express-validator/express-validator/edit/master/docs/',
-
-  /* custom fonts for website */
-  /*fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },*/
-
-  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
   copyright:
     'Copyright © ' +
     new Date().getFullYear() +
     ' express-validator',
-
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
     theme: 'default',
-  },
-
-  // Add custom scripts here that would be placed in <script> tags
-  scripts: ['https://buttons.github.io/buttons.js'],
-
-  /* On page navigation for the current documentation page */
-  onPageNav: 'separate'
+  }
 };
 
 module.exports = siteConfig;
