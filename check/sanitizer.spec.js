e71a8b475acf3e47cd8aa44770f83e2c829146fd
Graham Clifford <graham.clifford@beneoffice.com>
2018-09-19 08:13:14 -0400

    Fixed linting issues

--- check/sanitizer.spec.js
+++ check/sanitizer.spec.js
@@ -2,86 +2,86 @@ const { expect } = require('chai');
 const { check } = require('./validation-chain-builders');
 
 describe('.toInt()', () => {
-    it('A whole number as a string, becomes a number', () => {
-        const req = {
-          body: { foo: '88' }
-        };
-    
-        check('foo').toInt()(req, {}, () => {});
-        expect(req.body.foo).to.equal(88);
-    });
+  it('A whole number as a string, becomes a number', () => {
+    const req = {
+      body: { foo: '88' }
+    };
 
-    it('A whole number as a number is still a number', () => {
-        const req = {
-          body: { foo: 15 }
-        };
-    
-        check('foo').toInt()(req, {}, () => {});
-        expect(req.body.foo).to.equal(15);
-    });
+    check('foo').toInt()(req, {}, () => { });
+    expect(req.body.foo).to.equal(88);
+  });
 
-    it('A floating point number as a string, becomes a number', () => {
-        const req = {
-          body: { foo: '145.372' }
-        };
-    
-        check('foo').toInt()(req, {}, () => {});
-        expect(req.body.foo).to.equal(145);
-    });
+  it('A whole number as a number is still a number', () => {
+    const req = {
+      body: { foo: 15 }
+    };
 
-    it('A whole number as a number is still a number', () => {
-        const req = {
-          body: { foo: 77.87 }
-        };
-    
-        check('foo').toInt()(req, {}, () => {});
-        expect(req.body.foo).to.equal(77);
-    });
+    check('foo').toInt()(req, {}, () => { });
+    expect(req.body.foo).to.equal(15);
+  });
+
+  it('A floating point number as a string, becomes a number', () => {
+    const req = {
+      body: { foo: '145.372' }
+    };
+
+    check('foo').toInt()(req, {}, () => { });
+    expect(req.body.foo).to.equal(145);
+  });
+
+  it('A whole number as a number is still a number', () => {
+    const req = {
+      body: { foo: 77.87 }
+    };
+
+    check('foo').toInt()(req, {}, () => { });
+    expect(req.body.foo).to.equal(77);
+  });
 });
 
 describe('.toBoolean()', () => {
-    it('A string of "true", should be a boolean true', () => {
-        const req = {
-          body: { foo: 'true' }
-        };
-    
-        check('foo').toBoolean(true)(req, {}, () => {});
-        expect(req.body.foo).to.equal(true);
-    });
+  it('A string of "true", should be a boolean true', () => {
+    const req = {
+      body: { foo: 'true' }
+    };
 
-    it('A boolean of true, should be a boolean true', () => {
-        const req = {
-          body: { foo: true }
-        };
-    
-        check('foo').toBoolean(true)(req, {}, () => {});
-        expect(req.body.foo).to.equal(true);
-    });
+    check('foo').toBoolean(true)(req, {}, () => { });
+    expect(req.body.foo).to.equal(true);
+  });
 
-    it('A string of "false", should be a boolean false', () => {
-        const req = {
-          body: { foo: 'false' }
-        };
-    
-        check('foo').toBoolean(true)(req, {}, () => {});
-        expect(req.body.foo).to.equal(false);
-    });
+  it('A boolean of true, should be a boolean true', () => {
+    const req = {
+      body: { foo: true }
+    };
 
-    it('A boolean of false, should be a boolean false', () => {
-        const req = {
-          body: { foo: false }
-        };
-    
-        check('foo').toBoolean(true)(req, {}, () => {});
-        expect(req.body.foo).to.equal(false);
-    });
+    check('foo').toBoolean(true)(req, {}, () => { });
+    expect(req.body.foo).to.equal(true);
+  });
 
-    it('A number of 1234, should be a boolean of false', () => {
-        const req = {
-          body: { foo: 1234 }
-        };
-    
-        check('foo').toBoolean(true)(req, {}, () => {});
-        expect(req.body.foo).to.equal(false);
-    });
+  it('A string of "false", should be a boolean false', () => {
+    const req = {
+      body: { foo: 'false' }
+    };
+
+    check('foo').toBoolean(true)(req, {}, () => { });
+    expect(req.body.foo).to.equal(false);
+  });
+
+  it('A boolean of false, should be a boolean false', () => {
+    const req = {
+      body: { foo: false }
+    };
+
+    check('foo').toBoolean(true)(req, {}, () => { });
+    expect(req.body.foo).to.equal(false);
+  });
+
+  it('A number of 1234, should be a boolean of false', () => {
+    const req = {
+      body: { foo: 1234 }
+    };
+
+    check('foo').toBoolean(true)(req, {}, () => { });
+    expect(req.body.foo).to.equal(false);
+  });
 });
