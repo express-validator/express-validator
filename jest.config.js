28ae07f7509700450783da094d19ea24a5996b46
Gustavo Henke <guhenke@gmail.com>
2019-03-31 18:32:01 +1100

    jest: collect test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   testRegex: "src/.*\.spec\.ts",
   testEnvironment: "node",
-  preset: "ts-jest"
+  preset: "ts-jest",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/index.ts",
+    "!src/**/*.spec.ts"
+  ]
 };
