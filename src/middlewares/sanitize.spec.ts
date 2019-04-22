6b0b31354d1be626ffa0b90498e34061ff85e8bf
Gustavo Henke <guhenke@gmail.com>
2019-04-22 18:10:49 +1000

    middleware: implement sanitize

---
+++ src/middlewares/sanitize.spec.ts
@@ -0,0 +1,68 @@
+import { defaultRunners, sanitize } from './sanitize';
+import { PersistBack, Sanitize, EnsureInstance, RemoveOptionals, SelectFields, ContextRunner, FieldInstance } from '../context-runners';
+import { SanitizersImpl } from '../chain';
+
+// Some tests might change the list of runners, so we keep the original list and reset it afterwards
+const originalRunners = defaultRunners.slice();
+const overrideRunners = (newRunners: ({ new(): ContextRunner })[]) => {
+  defaultRunners.splice(0, defaultRunners.length, ...newRunners);
+};
+afterEach(() => {
+  overrideRunners(originalRunners);
+});
+
+it('has a default list of runners', () => {
+  expect(defaultRunners).toEqual([
+    SelectFields,
+    Sanitize,
+    RemoveOptionals,
+    EnsureInstance,
+    PersistBack,
+  ]);
+});
+
+it('has sanitizer methods', () => {
+  const chain = sanitize('foo');
+  Object.keys(SanitizersImpl.prototype).forEach(method => {
+    expect(chain).toHaveProperty(method);
+    expect(typeof (chain as any)[method]).toBe('function');
+  });
+});
+
+it('runs the default runners', done => {
+  const selectedFields: FieldInstance[] = [{
+    path: 'foo',
+    originalPath: 'foo',
+    location: 'body',
+    value: 123,
+    originalValue: 123,
+  }];
+
+  const runA = jest.fn().mockResolvedValue(selectedFields);
+  const runB = jest.fn().mockResolvedValue([]);
+  overrideRunners([class { run = runA; }, class { run = runB; }]);
+
+  const req = {
+    body: { foo: 123 },
+  };
+  const middleware = sanitize(['foo', 'bar'], ['body']);
+  expect(middleware.context.fields).toEqual(['foo', 'bar']);
+  expect(middleware.context.locations).toEqual(['body']);
+
+  middleware(req, {}, () => {
+    expect(runA).toHaveBeenCalledWith(req, middleware.context, []);
+    expect(runB).toHaveBeenCalledWith(req, middleware.context, selectedFields);
+
+    done();
+  });
+});
+
+it('passes unexpected errors down to other middlewares', done => {
+  const error = new Error();
+  overrideRunners([class { run = jest.fn().mockRejectedValue(error); }]);
+
+  sanitize('foo')({}, {}, (err?: Error) => {
+    expect(err).toBe(error);
+    done();
+  });
+});
---
+++ src/middlewares/sanitize.ts
@@ -0,0 +1,48 @@
+import {
+  SanitizersImpl,
+  SanitizationChain,
+} from '../chain';
+import { Context } from '../context';
+import { Location, InternalRequest } from '../base';
+import { SelectFields, Sanitize, PersistBack, EnsureInstance, RemoveOptionals, ContextRunner } from '../context-runners';
+import { bindAll } from './utils';
+
+// This list of runners is here so it can be checked/extended by tests
+export const defaultRunners: ({ new(): ContextRunner })[] = [
+  SelectFields,
+  Sanitize,
+  RemoveOptionals,
+  EnsureInstance,
+  PersistBack,
+];
+
+export function sanitize(
+  fields: string | string[],
+  locations: Location[] = [],
+): SanitizationChain {
+  const context = new Context(
+    Array.isArray(fields) ? fields : [fields],
+    locations,
+  );
+
+  const runners = defaultRunners.map(Runner => new Runner());
+
+  const middleware = async (req: InternalRequest, _res: any, next: (err?: any) => void) => {
+    try {
+      await runners.reduce(
+        async (instances, runner) => runner.run(req, context, await instances),
+        Promise.resolve([]),
+      );
+    } catch (err) {
+      return next(err);
+    }
+
+    next();
+  };
+
+  return Object.assign(
+    middleware,
+    bindAll(new SanitizersImpl(context, middleware as any)),
+    { context },
+  );
+}
