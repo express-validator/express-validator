2c31499ad98c44b71acfe6114605715b839aa12f
Gustavo Henke <guhenke@gmail.com>
2019-05-20 17:03:20 +1000

    jest: give .ts files priority over .js
    Otherwise build artifacts from `tsc` would generate
    generate noise in the coverage results.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  testRegex: 'src/.*.spec.ts',
+  testRegex: 'src/.*\\.spec\\.ts',
   testEnvironment: 'node',
   preset: 'ts-jest',
+  // TS takes precedence as we want to avoid build artifacts from being required instead of up-to-date .ts file.
+  moduleFileExtensions: ['ts', 'js', 'json'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts', '!src/**/*.spec.ts', '!src/**/*.d.ts'],
 };
