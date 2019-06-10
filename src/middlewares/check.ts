3986093cbad0698153a95f5f5906dca961b63722
Gustavo Henke <guhenke@gmail.com>
2019-06-10 16:24:37 +1000

    middlewares/check: use ContextRunner instead of self-contained logic

--- src/middlewares/check.ts
+++ src/middlewares/check.ts
@@ -1,65 +1,40 @@
-import { ContextHandlerImpl, SanitizersImpl, ValidationChain, ValidatorsImpl } from '../chain';
+import {
+  ContextHandlerImpl,
+  ContextRunnerImpl,
+  SanitizersImpl,
+  ValidationChain,
+  ValidatorsImpl,
+} from '../chain';
 import { Context } from '../context';
-import {
-  InternalRequest,
-  Location,
-  contextsSymbol,
-  errorsSymbol,
-  middlewareModeSymbol,
-} from '../base';
-import {
-  ContextRunner,
-  EnsureInstance,
-  PersistBack,
-  RemoveOptionals,
-  Sanitize,
-  SelectFields,
-  Validate,
-} from '../context-runners';
+import { InternalRequest, Location, contextsSymbol } from '../base';
 import { bindAll } from '../utils';
-
-// This list of runners is here so it can be checked/extended by tests
-export const defaultRunners: ({ new (): ContextRunner })[] = [
-  SelectFields,
-  Sanitize,
-  RemoveOptionals,
-  EnsureInstance,
-  PersistBack,
-  Validate,
-];
 
 export function check(
   fields: string | string[],
   locations: Location[] = [],
   message?: any,
 ): ValidationChain {
-  const context = new Context(Array.isArray(fields) ? fields : [fields], locations, message);
-
-  const runners = defaultRunners.map(Runner => new Runner());
+  const context = new Context(message);
+  const runner = new ContextRunnerImpl(
+    context,
+    Array.isArray(fields) ? fields : [fields],
+    locations,
+  );
 
   const middleware = async (req: InternalRequest, _res: any, next: (err?: any) => void) => {
+    req[contextsSymbol] = (req[contextsSymbol] || []).concat(context);
+
     try {
-      await runners.reduce(
-        async (instances, runner) => runner.run(req, context, await instances),
-        Promise.resolve([]),
-      );
-    } catch (err) {
-      // We hope to throw only errors from the Validate runner.
-      // Anything else means 💥
-      if (req[middlewareModeSymbol] || !Array.isArray(err)) {
-        return next(err);
-      }
-
-      req[errorsSymbol] = (req[errorsSymbol] || []).concat(err);
-    } finally {
-      req[contextsSymbol] = (req[contextsSymbol] || []).concat(context);
+      await runner.run(req);
+      next();
+    } catch (e) {
+      next(e);
     }
-
-    next();
   };
 
   return Object.assign(
     middleware,
+    bindAll(runner),
     bindAll(new SanitizersImpl(context, middleware as any)),
     bindAll(new ValidatorsImpl(context, middleware as any)),
     bindAll(new ContextHandlerImpl(context, middleware as any)),
