ba03746325ceffdf235150cef4f69b6744b219e4
Connor Fitzgerald <connor.jay.fitzgerald@outlook.com>
2019-08-19 12:02:48 +0100

    Fix options for optional

--- src/middlewares/schema.ts
+++ src/middlewares/schema.ts
@@ -32,7 +32,14 @@ type InternalParamSchema = ValidatorsSchema & SanitizersSchema;
 export type ParamSchema = InternalParamSchema & {
   in?: Location | Location[];
   errorMessage?: DynamicMessageCreator | any;
-  optional?: Partial<Optional> | true;
+  optional?: 
+    | true
+    | {
+        options?: {
+          checkFalsy?: boolean;
+          nullable?: boolean;
+        }
+      };
 };
 
 /**
