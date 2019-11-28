e3745ed7983b9f1f905457682c7a93c81d3ed6ea
Poyoman <big-bug@laposte.net>
2019-11-28 13:46:54 +0100

    Fix #803

--- src/context-items/custom-validation.ts
+++ src/context-items/custom-validation.ts
@@ -11,12 +11,9 @@ export class CustomValidation implements ContextItem {
     try {
       const result = this.validator(value, meta);
       const actualResult = await result;
-      const isPromise = result && result.then;
       const failed = this.negated ? actualResult : !actualResult;
-
-      // A promise that was resolved only adds an error if negated.
-      // Otherwise it always suceeds
-      if ((!isPromise && failed) || (isPromise && this.negated)) {
+      
+      if (failed) {
         context.addError(this.message, value, meta);
       }
     } catch (err) {
