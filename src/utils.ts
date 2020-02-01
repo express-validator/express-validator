9bfa0449baa68bfa1dc76bfe4a287d634f1a4bdc
Gustavo Henke <guhenke@gmail.com>
2020-02-01 23:07:58 +1100

    utils: don't bind constructor

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ export const bindAll = <T>(object: T): { [K in keyof T]: T[K] } => {
   const protoKeys = Object.getOwnPropertyNames(Object.getPrototypeOf(object)) as (keyof T)[];
   protoKeys.forEach(key => {
     const maybeFn = object[key];
-    if (typeof maybeFn === 'function') {
+    if (typeof maybeFn === 'function' && key !== 'constructor') {
       object[key] = maybeFn.bind(object);
     }
   });
