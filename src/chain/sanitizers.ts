dac6e4ac95e2b8eb5aaf182f6e3422cefc007b4f
Gustavo Henke <guhenke@gmail.com>
2020-11-15 23:29:16 +1100

    reorder/group sanitizers

--- src/chain/sanitizers.ts
+++ src/chain/sanitizers.ts
@@ -2,16 +2,18 @@ import { CustomSanitizer } from '../base';
 import * as Options from '../options';
 
 export interface Sanitizers<Return> {
+  // custom sanitizers
   customSanitizer(sanitizer: CustomSanitizer): Return;
-
-  blacklist(chars: string): Return;
   default(default_value: any): Return;
+  replace(values_to_replace: any, new_value: any): Return;
+
+  // validator's sanitizers
+  blacklist(chars: string): Return;
   escape(): Return;
   unescape(): Return;
   ltrim(chars?: string): Return;
   normalizeEmail(options?: Options.NormalizeEmailOptions): Return;
   rtrim(chars?: string): Return;
-  replace(values_to_replace: any, new_value: any): Return;
   stripLow(keep_new_lines?: boolean): Return;
   toArray(): Return;
   toBoolean(strict?: boolean): Return;
