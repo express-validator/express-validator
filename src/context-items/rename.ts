7a1f079d3297ae897c007735b3a90b6873a492fd
Federico Ciardi <fed.ciardi@gmail.com>
2021-01-01 18:23:06 +0100

    feat: add 'Rename' context-item

---
+++ src/context-items/rename.ts
@@ -0,0 +1,30 @@
+import * as _ from 'lodash';
+import { Meta } from '../base';
+import { Context } from '../context';
+import { ContextItem } from './context-item';
+
+export class Rename implements ContextItem {
+  constructor(readonly newPath: string) {}
+
+  async run(context: Context, value: any, meta: Meta) {
+    const { req, location, path } = meta;
+
+    if (path !== this.newPath) {
+      if (context.fields.length !== 1) {
+        throw new Error('Cannot rename multiple fields.');
+      }
+      const field = context.fields[0];
+      if (field.includes('*') || this.newPath.includes('*')) {
+        throw new Error('Cannot use rename() with wildcards.');
+      }
+
+      if (_.get(req[location], this.newPath) !== undefined) {
+        throw new Error(`Cannot rename to req.${location}.${path} as it already exists.`);
+      }
+
+      _.set(req[location], this.newPath, value);
+      _.unset(req[location], path);
+    }
+    return Promise.resolve();
+  }
+}
