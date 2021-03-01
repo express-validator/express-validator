42e1af4c35e5f8b5460ada59776b934d55a22f44
Federico Ciardi <fed.ciardi@gmail.com>
2021-03-01 18:58:12 +0100

    breaking: remove deprecated APIs (#993)

--- src/chain/sanitization-chain.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from '../base';
-import { ContextBuilder } from '../context-builder';
-import { Sanitizers } from './sanitizers';
-import { ContextRunner } from './context-runner';
-
-export interface SanitizationChain extends Sanitizers<SanitizationChain>, ContextRunner {
-  (req: Request, res: any, next: (errors?: any) => void): void;
-  builder: ContextBuilder;
-}
