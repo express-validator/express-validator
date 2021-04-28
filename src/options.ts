c68491222579766876d71d9c187bf172a0057122
trumankain <42022383+trumankain@users.noreply.github.com>
2021-04-28 12:57:23 -0700

    Add IsAlphanumericOptions

--- src/options.ts
+++ src/options.ts
@@ -397,6 +397,10 @@ export interface IsAlphaOptions {
   ignore?: string | string[] | RegExp;
 }
 
+export interface IsAlphanumericOptions {
+  ignore?: string | string[] | RegExp;
+}
+
 /**
  * defaults to
  * {
