ac65aed84bcaafe1b9aff29c4786eb061540dee2
Federico Ciardi <fed.ciardi@gmail.com>
2021-09-26 15:46:07 +0200

    feat(oneOf): add new error types (#1022)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testRegex: 'src/.*\\.spec\\.ts',
+  testRegex: 'src/.*\\.spec\\.ts$',
   testEnvironment: 'node',
   preset: 'ts-jest',
   // TS takes precedence as we want to avoid build artifacts from being required instead of up-to-date .ts file.
