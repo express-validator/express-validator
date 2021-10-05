4927a2273293d8aef28dd118eb7b5bd79497ecec
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-10-05 18:29:52 -0300

    refactor: remove import not used

--- filter/index.ts
+++ filter/index.ts
@@ -1,18 +1,14 @@
 export {
-  // base
   Location,
   Meta,
   CustomSanitizer,
-  // chain
   SanitizationChain,
-  // middleware/sanitization-chain-builders
   sanitize,
   sanitizeBody,
   sanitizeCookie,
   sanitizeParam,
   sanitizeQuery,
   buildSanitizeFunction,
-  // matched-data
   matchedData,
   MatchedDataOptions,
 } from '../src';
