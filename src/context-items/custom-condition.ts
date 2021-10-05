8f6e0dabb9ec7f70cb5fd3d9c7ff5f35a4a96b0b
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-10-05 13:47:05 -0300

    refactor: remove the unnecessary code

--- src/context-items/custom-condition.ts
+++ src/context-items/custom-condition.ts
@@ -7,8 +7,7 @@ export class CustomCondition implements ContextItem {
 
   async run(_context: Context, value: any, meta: Meta) {
     try {
-      const result = this.condition(value, meta);
-      await result;
+      const result = await this.condition(value, meta);
 
       // if the promise resolved or the result is truthy somehow, then there's no validation halt.
       if (!result) {
