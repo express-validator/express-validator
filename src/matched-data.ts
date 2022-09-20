67b26eba6070794fe25576316da6df9b8920f9e6
Andrew Barba <barba@hey.com>
2022-09-20 16:27:29 -0400

    Cleanup

--- src/matched-data.ts
+++ src/matched-data.ts
@@ -43,13 +43,14 @@ function createValidityFilter(onlyValidData = true) {
   return !onlyValidData
     ? () => true
     : (field: FieldInstanceBag) => {
+        const isValidNullable =
+          field.context.optional && field.context.optional.nullable && field.instance.value == null;
+
         const hasError = field.context.errors.some(
           error =>
             error.location === field.instance.location &&
             error.param === field.instance.path &&
-            (!field.context.optional ||
-              !field.context.optional.nullable ||
-              field.instance.value != null),
+            !isValidNullable,
         );
 
         return !hasError;
