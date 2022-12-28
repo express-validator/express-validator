ce77e7733c8d584c78623f200a956885ad4c165d
Gustavo Henke <guhenke@gmail.com>
2022-12-29 01:24:01 +1100

    Add tests specific to toString

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,33 +1,18 @@
 import { toString } from './utils';
 
 describe('#toString', () => {
-  it('arrays should be converted to a single string', () => {
-    const value = ['foo', 1, false];
-    expect(toString(value)).toEqual('foo,1,false');
-  });
-
-  it('calls custom toString function', () => {
-    const value = {
-      toString() {
-        return 'foo';
-      },
-    };
-    expect(toString(value)).toEqual('foo');
-  });
-
-  it('calls Object.prototype.toString() when custom one is not function', () => {
-    const value = {
-      toString: 'something',
-    };
-
-    expect(toString(value)).toEqual('[object Object]');
-  });
-
-  it('calls Object.prototype.toString() when passed object does not have a toString function', () => {
-    const value = {
-      foo: 'foo',
-    };
-
-    expect(toString(value)).toEqual('[object Object]');
+  it.each([
+    ['boolean', false, 'false'],
+    ['null', null, ''],
+    ['undefined', undefined, ''],
+    ['NaN', NaN, ''],
+    // new Date(Date.UTC()) makes sure we'll not have to deal with timezones
+    ['Date object', new Date(Date.UTC(2019, 4, 1, 10, 30, 50, 0)), '2019-05-01T10:30:50.000Z'],
+    ['object with custom toString function', { toString: () => 'foo' }, 'foo'],
+    ["object with toString that's not a function", { toString: 'something' }, '[object Object]'],
+    ['object without custom toString', {}, '[object Object]'],
+    ['array', [1, 2, 3], '1,2,3'],
+  ])('on %s', (_description, input, expected) => {
+    expect(toString(input)).toBe(expected);
   });
 });
