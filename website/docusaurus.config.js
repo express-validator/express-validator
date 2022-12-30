5360e550a389483ba3822fd353ceecf58b8b53be
Gustavo Henke <guhenke@gmail.com>
2022-12-30 12:36:15 +1100

    Use "typescript" in the config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,22 +1,109 @@
+// @ts-check
+
 const REPO_URL = 'https://github.com/express-validator/express-validator';
 
-// https://docusaurus.io/docs/api/docusaurus-config
-const siteConfig = {
-  presets: [
-    [
-      '@docusaurus/preset-classic',
+/** @type {import('@docusaurus/preset-classic').Options} */
+const presetConfig = {
+  theme: {
+    customCss: require.resolve('./static/css/custom.css'),
+  },
+  docs: {
+    path: '../docs/',
+    sidebarPath: require.resolve('./sidebars.json'),
+    editUrl: `${REPO_URL}/edit/master/docs/`,
+  },
+};
+
+/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+const themeConfig = {
+  // algolia: {
+  //   apiKey: 'd6d85148b3b81778cf952442d4292bea',
+  //   indexName: 'express-validator',
+  //   algoliaOptions: {
+  //     facetFilters: ['type:lvl3', 'language:LANGUAGE', 'version:VERSION'],
+  //   },
+  // },
+  docs: {
+    sidebar: {
+      autoCollapseCategories: false,
+    },
+  },
+  navbar: {
+    logo: {
+      src: 'img/logo.svg',
+    },
+    items: [
+      { type: 'doc', docId: 'index', label: 'Docs', position: 'left' },
+      { type: 'doc', docId: 'check-api', label: 'API', position: 'left' },
       {
-        theme: {
-          customCss: require.resolve('./static/css/custom.css'),
-        },
-        docs: {
-          path: '../docs/',
-          sidebarPath: require.resolve('./sidebars.json'),
-          editUrl: `${REPO_URL}/edit/master/docs/`,
-        },
+        type: 'docsVersionDropdown',
+        position: 'right',
+      },
+      {
+        href: REPO_URL,
+        label: 'GitHub',
+        position: 'right',
       },
     ],
-  ],
+  },
+  footer: {
+    style: 'dark',
+    logo: {
+      src: 'img/logo.svg',
+    },
+    links: [
+      {
+        title: 'Docs',
+        items: [
+          {
+            label: 'Getting Started',
+            to: 'docs',
+          },
+          {
+            label: 'API Reference',
+            to: 'docs/check-api',
+          },
+        ],
+      },
+      {
+        title: 'Community',
+        items: [
+          {
+            label: 'StackOverflow',
+            href: 'http://stackoverflow.com/questions/tagged/express-validator',
+          },
+        ],
+      },
+      {
+        title: 'More',
+        items: [
+          {
+            label: 'GitHub',
+            href: 'https://github.com/express-validator/express-validator',
+          },
+          {
+            html: `<a
+                  class="github-button footer__link-item" href=${REPO_URL}
+                  data-icon="octicon-star"
+                  data-count-href="/express-validator/express-validator/stargazers"
+                  data-show-count={true}
+                  data-count-aria-label="# stargazers on GitHub"
+                  aria-label="Star this project on GitHub"
+                >
+                  Star
+                </a>`,
+          },
+        ],
+      },
+    ],
+    copyright: 'Copyright © ' + new Date().getFullYear() + ' express-validator',
+  },
+};
+
+// https://docusaurus.io/docs/api/docusaurus-config
+/** @type {import('@docusaurus/types').Config} */
+const siteConfig = {
+  presets: [['@docusaurus/preset-classic', presetConfig]],
   plugins: [
     [
       '@docusaurus/plugin-client-redirects',
@@ -37,85 +124,7 @@ const siteConfig = {
   // Website customization
   favicon: 'img/favicon.png',
   scripts: ['https://buttons.github.io/buttons.js'],
-  themeConfig: {
-    // algolia: {
-    //   apiKey: 'd6d85148b3b81778cf952442d4292bea',
-    //   indexName: 'express-validator',
-    //   algoliaOptions: {
-    //     facetFilters: ['type:lvl3', 'language:LANGUAGE', 'version:VERSION'],
-    //   },
-    // },
-    navbar: {
-      logo: {
-        src: 'img/logo.svg',
-      },
-      items: [
-        { type: 'doc', docId: 'index', label: 'Docs', position: 'left' },
-        { type: 'doc', docId: 'check-api', label: 'API', position: 'left' },
-        {
-          type: 'docsVersionDropdown',
-          position: 'right',
-        },
-        {
-          href: REPO_URL,
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      logo: {
-        src: 'img/logo.svg',
-      },
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Getting Started',
-              to: 'docs',
-            },
-            {
-              label: 'API Reference',
-              to: 'docs/check-api',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'StackOverflow',
-              href: 'http://stackoverflow.com/questions/tagged/express-validator',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/express-validator/express-validator',
-            },
-            {
-              html: `<a
-                  class="github-button footer__link-item" href=${REPO_URL}
-                  data-icon="octicon-star"
-                  data-count-href="/express-validator/express-validator/stargazers"
-                  data-show-count={true}
-                  data-count-aria-label="# stargazers on GitHub"
-                  aria-label="Star this project on GitHub"
-                >
-                  Star
-                </a>`,
-            },
-          ],
-        },
-      ],
-      copyright: 'Copyright © ' + new Date().getFullYear() + ' express-validator',
-    },
-  },
+  themeConfig,
 };
 
 module.exports = siteConfig;
