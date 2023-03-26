8f6820c075270db2c049d9d42e38d64b3dc7454f
Gustavo Henke <guhenke@gmail.com>
2023-03-26 23:24:24 +1100

    docs: add page for oneOf() and friends

---
+++ website/components/example.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './example.module.css';
+
+export const SideBySideExample = ({ children }) => {
+  return <div className={styles.example}>{children}</div>;
+};
+
+export function UsageExample({ children }) {
+  return (
+    <div>
+      <strong className={styles.exampleTitle}>Usage example</strong>
+      {children}
+    </div>
+  );
+}
+
+export function ErrorExample({ children }) {
+  return (
+    <div>
+      <strong className={styles.exampleTitle}>Error example</strong>
+      {children}
+    </div>
+  );
+}
